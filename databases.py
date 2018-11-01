5539ad2e2a745b13b9b17593a77f16e56b808db4
aboud20-meet <aboud20@meet.mit.edu>
2018-11-01 18:43:26 +0200

    session 4

--- databases.py
+++ databases.py
@@ -10,16 +10,35 @@ session = DBSession()
 
 # Write your functions to interact with the database here :
 
-def create_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+def create_product(name,price,quantity,description):
+    product_object = Product(
+        name=name,
+        price=price,
+        quantity=quantity,
+        description=description)
+    session.add(product_object)
+    session.commit()
 
-def update_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
 
-def delete_product(id):
-  pass
+def update_product(name,price,quantity):
+  #TODO: complete the functions (you will need to change the function's inputs)
+	product_object = session.query(
+		Product).filter_by(
+		name=name).first()
+	if(price>300):
+		print("error price is too high")
+	else:
+		product_object.price = price
+	product_object.quantity = quantity
+	session.commit()
+
+
+
+
+def delete_product(their_name):
+	session.query(Product).filter_by(
+       name=their_name).delete()
+	session.commit()
 
 def get_product(id):
   pass
